11258dc9d6ec4b81daaac6ecb07a08d67d12eda9
mikey <milehimikey@gmail.com>
2025-04-17 23:54:40 -0600

    Move queries

--- src/main/kotlin/wtf/milehimikey/axondashboardjpa/model/Queries.kt
+++ src/main/kotlin/wtf/milehimikey/axondashboardjpa/model/Queries.kt
@@ -1,13 +1,6 @@
-package wtf.milehimikey.axondashboardjpa
+package wtf.milehimikey.axondashboardjpa.model
 
 import org.springframework.data.jpa.repository.JpaRepository
-import wtf.milehimikey.axondashboardjpa.model.DeadLetterEntry
-import wtf.milehimikey.axondashboardjpa.model.DomainEventEntry
-import wtf.milehimikey.axondashboardjpa.model.SagaEntry
-import wtf.milehimikey.axondashboardjpa.model.SnapshotEventEntry
-import wtf.milehimikey.axondashboardjpa.model.SnapshotEventEntryId
-import wtf.milehimikey.axondashboardjpa.model.TokenEntry
-import wtf.milehimikey.axondashboardjpa.model.TokenEntryId
 
 interface DomainEventEntryRepository: JpaRepository<DomainEventEntry, Long> {
 }
